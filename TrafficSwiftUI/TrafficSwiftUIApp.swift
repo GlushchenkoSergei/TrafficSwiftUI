3b180e4cade1832ceca1f389290e904e8ecb0905
Sergei <glushenko6314@icloud.com>
2022-05-26 20:36:16 +0300

    Initial Commit

---
+++ TrafficSwiftUI/TrafficSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficSwiftUIApp.swift
+//  TrafficSwiftUI
+//
+//  Created by mac on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
